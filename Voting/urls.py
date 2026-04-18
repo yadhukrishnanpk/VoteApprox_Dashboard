04ad436ab73edf69da143517e839b14978e64aec
Yadhukrishnan Pk <yadhukrishnanpkotml02@gmail.com>
2026-04-19 00:09:57 +0530

    Dashboard created

--- Voting/urls.py
+++ Voting/urls.py
@@ -3,7 +3,14 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('election/<int:election_id>/', views.index, name='index_with_id'),
     path('votelist/', views.voters_list, name='voterlist'),
     path('candidatelist/', views.candidate_list, name='candidatelist'),
     path('partylist/', views.party_list, name='partylist'),
+    path('elections/', views.election_list, name='electionlist'),
+    
+    
+    path('voters/<int:election_id>/', views.voters_list, name='voterlist'),
+    path('candidates/<int:election_id>/', views.candidate_list, name='candidatelist'),
+    path('parties/<int:election_id>/', views.party_list, name='partylist'),
 ]
--- Voting/views.py
+++ Voting/views.py
@@ -1,71 +1,121 @@
-from django.shortcuts import render
-from .models import Candidate,Vote,Voter,Party,Election
-from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
+from .models import Candidate, Vote, Voter, Party, Election
+from django.db.models import Count, Q
 
-
-def index(request):
+# Helper function to avoid repeating code in every view
+def get_election_data(election_id):
     
-    candidate_count = Candidate.objects.count()
-    voter_count = Voter.objects.count()
-    party_count = Party.objects.count()
-    election_count = Election.objects.count()
-    candidates_list = Candidate.objects.annotate(received_votes=Count('vote', distinct=True))
-    total_votes_cast = sum(c.received_votes for c in candidates_list)
+    all_elections = Election.objects.all()
+    if election_id:
+        current_election = get_object_or_404(Election, id=election_id)
+    else:
+        current_election = all_elections.first()
+    return current_election, all_elections
 
-    for person in candidates_list:
-        if total_votes_cast > 0:
-            # Math: (1 / 100) * 100 = 1%
-            person.vote_percentage = (float(person.received_votes) / float(total_votes_cast)) * 100
-        else:
-            person.vote_percentage = 0
+def index(request, election_id=None):
+    
+    
+    current_election, all_elections = get_election_data(election_id)
+    
+    voter_count = Voter.objects.all().count
+    party_count = Party.objects.all().count
+    election_count = all_elections.count()
+    voter=Voter.objects.count()
+
+    if current_election:
+        data_list = current_election.candidates.annotate(
+            received_votes=Count('vote', filter=Q(vote__election=current_election))
+        )
+        total_votes_in_this_election = Vote.objects.filter(election=current_election).count()
+        candidate_count = current_election.candidates.count()
+
+        for person in data_list:
+            if voter > 0:
+                # Math: (1 vote / 100 total voters) * 100 = 1%
+                person.vote_percentage = round(float(person.received_votes), 1)
+            else:
+                person.vote_percentage = 0
+    else:
+        data_list = []
+        total_votes_in_this_election = 0
+        candidate_count = 0
+
     context = {
-        'candidate_count': candidate_count,
-        'vote_count': total_votes_cast, 
+        'election': current_election,
+        'all_elections': all_elections,
+        'data_list': data_list,
+        'vote_count': total_votes_in_this_election, 
         'voter_count': voter_count,
         'party_count': party_count,
+        'candidate_count': candidate_count,
         'election_count': election_count,
-        'total_votes': total_votes_cast,
-        'vote_count': Vote.objects.count(),
-        'data_list': candidates_list, 
     }
     return render(request, "index.html", context)
 
-def voters_list(request):
-    VoterS =Voter.objects.all()
-    voters = Voter.objects.prefetch_related('vote_set__candidate__party').all()
+def voters_list(request, election_id=None):
+    current_election, all_elections = get_election_data(election_id)
+    
+    # Get EVERY voter in the database
+    voters = Voter.objects.all().prefetch_related('vote_set__candidate__party')
+    
     for voter in voters:
-        vote_record = voter.vote_set.first()
+        # Look for a vote record specifically for THIS election
+        vote_record = voter.vote_set.filter(election=current_election).first()
         if vote_record:
             voter.voted_for_party = vote_record.candidate.party.name
             voter.voted_for_candidate = vote_record.candidate.name
         else:
-            voter.voted_for_party = "N/A"
-            voter.voted_for_candidate = "N/A"
+            voter.voted_for_party = "Not Voted"
+            voter.voted_for_candidate = "Not Voted"
     
-    context={
-        'datas': VoterS,
-        'voters': voters
-        
-    }
-    return render(request, "voter_list.html",context)
+    return render(request, "voter_list.html", {
+        'voters': voters, 
+        'all_elections': all_elections, 
+        'election': current_election
+    })
 
-
-def candidate_list(request):
+def candidate_list(request, election_id=None):
+    addcandidate=Candidate.objects.all()
+    current_election, all_elections = get_election_data(election_id)
     
-    candidate=Candidate.objects.all()
+    # Filter: Only show candidates registered for this election
+    if current_election:
+        candidates = current_election.candidates.all()
+    else:
+        candidates = []
     
-    context={
-        'candidate': candidate
+    context = {
+        'all_elections': all_elections,
+        'election': current_election,
+        'candidate': candidates,
+        'addcandidate': addcandidate
     }
-    
     return render(request, "candidatelist.html", context)
 
-def party_list(request):
+def party_list(request, election_id=None):
+    addparty=Party.objects.all()
+    current_election, all_elections = get_election_data(election_id)
     
-    party=Party.objects.all()
+    if current_election:
+        # Change 'candidate' to 'candidates'
+        parties = Party.objects.filter(candidates__in=current_election.candidates.all()).distinct()
+    else:
+        parties = []
     
-    context={
-        'party': party
+    context = {
+        'all_elections': all_elections,
+        'election': current_election,
+        'party': parties,
+        'add': addparty,
+        
     }
-    
     return render(request, "partylist.html", context)
+
+def election_list(request):
+    all_elections = Election.objects.all() 
+    # For the general list, we don't necessarily need a "current" election filter
+    context = {
+        'all_elections': all_elections,
+        'elections': all_elections, 
+    }
+    return render(request, "electionlist.html", context)
