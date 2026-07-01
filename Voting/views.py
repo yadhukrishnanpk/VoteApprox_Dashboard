708edc870accf28991dda40703717211020e961a
YADHUKRISHNAN PK <yadhukrishnanpkotml02@gmail.com>
2026-07-01 19:23:38 +0530

    Login Error fixing

--- Voting/views.py
+++ Voting/views.py
@@ -357,20 +357,24 @@ def register(request):
     return render(request, "register.html", context)
 
 def login(request):
-    if request.method =='POST':
-        form = AuthenticationForm(request,request.POST)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username =form.cleaned_data['username']
-            password =form.cleaned_data['password']
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
 
-            user =auth.authenticate(username=username,password=password)
+            user = auth.authenticate(username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-            return redirect('index')
-        return redirect('register')
+                return redirect('index') 
+            else:
+                
+                form.add_error(None, "Invalid username or password.")
+        context = {'form': form}
+        return render(request, "login.html", context)
         
     form = AuthenticationForm()
-    context={
+    context = {
         'form': form,
     }
     return render(request, "login.html", context)
