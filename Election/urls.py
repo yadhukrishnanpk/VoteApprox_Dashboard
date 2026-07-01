e716b860e85f3e09c3fb585c867d97a1053de5f3
YADHUKRISHNAN PK <yadhukrishnanpkotml02@gmail.com>
2026-07-01 20:35:58 +0530

    Template for Server Error

--- Election/urls.py
+++ Election/urls.py
@@ -9,3 +9,8 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler400 = 'django.views.defaults.bad_request'
+handler403 = 'django.views.defaults.permission_denied'
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
